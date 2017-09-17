55a9a78291d429f78c9281969f61cd0a5cbfa3d5
sanjay kranthi <kranthi0987@gmail.com>
2017-09-17 21:41:09 +0530

    removed the help

--- app/src/main/java/com/sanjay/dotcamera/AsciiCamActivity.java
+++ app/src/main/java/com/sanjay/dotcamera/AsciiCamActivity.java
@@ -2,26 +2,6 @@
 
 package com.sanjay.dotcamera;
 
-import java.io.File;
-import java.io.IOException;
-import java.util.EnumMap;
-import java.util.Map;
-
-import com.google.android.gms.ads.AdListener;
-import com.google.android.gms.ads.AdRequest;
-import com.google.android.gms.ads.AdView;
-import com.google.android.gms.ads.InterstitialAd;
-import com.google.firebase.analytics.FirebaseAnalytics;
-import com.google.firebase.appindexing.Action;
-import com.google.firebase.appindexing.FirebaseUserActions;
-import com.google.firebase.appindexing.builders.Actions;
-import com.sanjay.dotcamera.AsciiConverter.ColorType;
-import com.sanjay.util.ARManager;
-import com.sanjay.util.AndroidUtils;
-import com.sanjay.util.AsyncProcessor;
-import com.sanjay.util.CameraUtils;
-import com.sanjay.util.ShutterButton;
-
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.app.AlertDialog;
@@ -49,51 +29,37 @@ import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.Toast;
 
+import com.google.android.gms.ads.AdListener;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.InterstitialAd;
+import com.google.firebase.analytics.FirebaseAnalytics;
+import com.sanjay.dotcamera.AsciiConverter.ColorType;
+import com.sanjay.util.ARManager;
+import com.sanjay.util.AndroidUtils;
+import com.sanjay.util.AsyncProcessor;
+import com.sanjay.util.CameraUtils;
+import com.sanjay.util.ShutterButton;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.EnumMap;
+import java.util.Map;
+
 public class AsciiCamActivity extends Activity
 implements Camera.PreviewCallback, ShutterButton.OnShutterButtonListener {
 
+    final static int ACTIVITY_PREFERENCES = 1;
+    final static int ACTIVITY_PICK_IMAGE = 2;
     private final static boolean DEBUG = false;
     private final static String TAG = "AsciiCamActivity";
-    private AdView mAdView;
-    private FirebaseAnalytics mFirebaseAnalytics;
     public InterstitialAd interAd = new InterstitialAd(this);
-
-    /**
-     * ATTENTION: This was auto-generated to implement the App Indexing API.
-     * See https://g.co/AppIndexing/AndroidStudio for more information.
-     */
-
-
-
-    // Pixels and metadata for a preview frame.
-    static class CameraPreviewData {
-        public final Camera camera;
-        public final CameraUtils.CameraInfo cameraInfo;
-        public final byte[] pixelData;
-        public final int width;
-        public final int height;
-        public final long timestamp;
-        public CameraPreviewData(Camera c, CameraUtils.CameraInfo i, byte[] p, int w, int h, long t) {
-            camera = c;
-            cameraInfo = i;
-            pixelData = p;
-            width = w;
-            height = h;
-            timestamp = t;
-        }
-    }
-
     ARManager arManager;
     boolean hasCameraPermission = false;
     AsciiConverter asciiConverter = new AsciiConverter();
     AsciiConverter.Result asciiResult = new AsciiConverter.Result();
-
     ColorType colorType = ColorType.ANSI_COLOR;
     Map<ColorType, String> pixelCharsMap = new EnumMap<ColorType, String>(ColorType.class);
-
-    final static int ACTIVITY_PREFERENCES = 1;
-    final static int ACTIVITY_PICK_IMAGE = 2;
-
     ImageButton cycleColorButton;
     ImageButton switchCameraButton;
     ImageButton settingsButton;
@@ -104,20 +70,61 @@ implements Camera.PreviewCallback, ShutterButton.OnShutterButtonListener {
     SurfaceView cameraView;
     OverlayView overlayView;
     LinearLayout verticalButtonBar;
-
     Handler handler = new Handler();
     boolean cameraViewReady = false;
     boolean appVisible = false;
     boolean saveInProgress = false;
-
     AsciiRenderer imageRenderer = new AsciiRenderer();
     AsciiImageWriter imageWriter = new AsciiImageWriter();
-
     AsyncProcessor<CameraPreviewData, Bitmap> imageProcessor;
     // If imageProcessor is busy when a preview frame arrives, store it here so that when it
     // finishes the current frame it can immediately process the next one without having to
     // wait until another frame arrives.
     CameraPreviewData nextPreviewData = null;
+    // Creates the bitmap to display from a camera preview frame.
+    AsyncProcessor.Producer<CameraPreviewData, Bitmap> asciiProducer =
+            new AsyncProcessor.Producer<CameraPreviewData, Bitmap>() {
+                @Override
+                public Bitmap processInput(CameraPreviewData input) {
+                    AsciiConverter.Orientation orientation = input.cameraInfo.isRotated180Degrees() ?
+                            AsciiConverter.Orientation.ROTATED_180 : AsciiConverter.Orientation.NORMAL;
+
+                    synchronized (asciiResult) {
+                        imageRenderer.setMaximumImageSize(overlayView.getWidth(), overlayView.getHeight());
+                        imageRenderer.setCameraImageSize(input.width, input.height);
+                        asciiConverter.computeResultForCameraData(input.pixelData, input.width, input.height,
+                                imageRenderer.asciiRows(), imageRenderer.asciiColumns(),
+                                colorType, pixelCharsMap.get(colorType), orientation, asciiResult);
+                        return imageRenderer.createBitmap(asciiResult);
+                    }
+                }
+            };
+    // Callback to handle an error producing a Bitmap.
+    AsyncProcessor.ErrorCallback<CameraPreviewData> errorCallback =
+            new AsyncProcessor.ErrorCallback<CameraPreviewData>() {
+                @Override
+                public void handleException(CameraPreviewData input, Exception ex) {
+                    Log.e(TAG, "Exception creating ascii image", ex);
+                    finishFrame(input);
+                }
+            };
+    // Callback to display the produced Bitmap.
+    AsyncProcessor.SuccessCallback<CameraPreviewData, Bitmap> successCallback =
+            new AsyncProcessor.SuccessCallback<CameraPreviewData, Bitmap>() {
+                @Override
+                public void handleResult(CameraPreviewData input, Bitmap output) {
+                    overlayView.setFlipHorizontal(input.cameraInfo.isFrontFacing());
+                    overlayView.setBitmap(output);
+                    overlayView.invalidate();
+                    if (DEBUG) {
+                        long processingMillis = System.currentTimeMillis() - input.timestamp;
+                        Log.i("AsciiCam", "Processed frame in : " + processingMillis + "ms");
+                    }
+                    finishFrame(input);
+                }
+            };
+    private AdView mAdView;
+    private FirebaseAnalytics mFirebaseAnalytics;
 
     /** Called when the activity is first created. */
     @Override public void onCreate(Bundle savedInstanceState) {
@@ -127,16 +134,16 @@ implements Camera.PreviewCallback, ShutterButton.OnShutterButtonListener {
         mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
         setContentView(R.layout.main);
 
-        cameraView = (SurfaceView)findViewById(R.id.cameraView);
-        overlayView = (OverlayView)findViewById(R.id.overlayView);
-        verticalButtonBar = (LinearLayout)findViewById(R.id.verticalButtonBar);
-        cycleColorButton = (ImageButton)findViewById(R.id.cycleColorButton);
-        switchCameraButton = (ImageButton)findViewById(R.id.switchCameraButton);
-        settingsButton = (ImageButton)findViewById(R.id.settingsButton);
-        galleryButton = (ImageButton)findViewById(R.id.galleryButton);
-        convertPictureButton = (ImageButton)findViewById(R.id.convertPictureButton);
-        helpButton = (ImageButton)findViewById(R.id.helpButton);
-        shutterButton = (ShutterButton)findViewById(R.id.shutterButton);
+        cameraView = findViewById(R.id.cameraView);
+        overlayView = findViewById(R.id.overlayView);
+        verticalButtonBar = findViewById(R.id.verticalButtonBar);
+        cycleColorButton = findViewById(R.id.cycleColorButton);
+        switchCameraButton = findViewById(R.id.switchCameraButton);
+        settingsButton = findViewById(R.id.settingsButton);
+        galleryButton = findViewById(R.id.galleryButton);
+        convertPictureButton = findViewById(R.id.convertPictureButton);
+//        helpButton = (ImageButton)findViewById(R.id.helpButton);
+        shutterButton = findViewById(R.id.shutterButton);
         shutterButton.setOnShutterButtonListener(this);
 
         arManager = ARManager.createAndSetupCameraView(this, cameraView, this);
@@ -147,7 +154,7 @@ implements Camera.PreviewCallback, ShutterButton.OnShutterButtonListener {
         switchCameraButton.setVisibility(CameraUtils.numberOfCameras() > 1 ? View.VISIBLE : View.GONE);
         updateFromPreferences();
         updateButtonsAndBackground();
-        mAdView = (AdView) findViewById(R.id.adView);
+        mAdView = findViewById(R.id.adView);
         AdRequest adRequest = new AdRequest.Builder().build();
         mAdView.loadAd(adRequest);
 
@@ -171,11 +178,13 @@ implements Camera.PreviewCallback, ShutterButton.OnShutterButtonListener {
         } , 600000);
 
     }
+
     public void displayinterstitial()
     {
         if (interAd.isLoaded())
         {interAd.show();}
     }
+
     @Override public void onPause() {
         appVisible = false;
         arManager.stopCamera();
@@ -405,6 +414,10 @@ implements Camera.PreviewCallback, ShutterButton.OnShutterButtonListener {
             Log.e(TAG, "Error updating color button", ex);
         }
     }
+//about
+//    public void onClick_gotoAbout(View view) {
+//        AboutActivity.startIntent(this);
+//    }
 
     // onClick_ methods are assigned as onclick handlers in the main.xml layout
     public void onClick_cycleColorMode(View view) {
@@ -423,10 +436,6 @@ implements Camera.PreviewCallback, ShutterButton.OnShutterButtonListener {
                 imageWriter.getBasePictureDirectory(), imageWriter.getThumbnailDirectory());
         startActivity(intent);
     }
-//about
-//    public void onClick_gotoAbout(View view) {
-//        AboutActivity.startIntent(this);
-//    }
 
     public void onClick_gotoPreferences(View view) {
         AsciiCamPreferences.startIntent(this, ACTIVITY_PREFERENCES);
@@ -441,48 +450,6 @@ implements Camera.PreviewCallback, ShutterButton.OnShutterButtonListener {
         Intent i = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
         startActivityForResult(i, ACTIVITY_PICK_IMAGE);
     }
-
-    // Creates the bitmap to display from a camera preview frame.
-    AsyncProcessor.Producer<CameraPreviewData, Bitmap> asciiProducer =
-            new AsyncProcessor.Producer<CameraPreviewData, Bitmap>() {
-        @Override public Bitmap processInput(CameraPreviewData input) {
-            AsciiConverter.Orientation orientation = input.cameraInfo.isRotated180Degrees() ?
-                    AsciiConverter.Orientation.ROTATED_180 : AsciiConverter.Orientation.NORMAL;
-
-            synchronized (asciiResult) {
-                imageRenderer.setMaximumImageSize(overlayView.getWidth(), overlayView.getHeight());
-                imageRenderer.setCameraImageSize(input.width, input.height);
-                asciiConverter.computeResultForCameraData(input.pixelData, input.width, input.height,
-                        imageRenderer.asciiRows(), imageRenderer.asciiColumns(),
-                        colorType, pixelCharsMap.get(colorType), orientation, asciiResult);
-                return imageRenderer.createBitmap(asciiResult);
-            }
-        }
-    };
-
-    // Callback to display the produced Bitmap.
-    AsyncProcessor.SuccessCallback<CameraPreviewData, Bitmap> successCallback =
-            new AsyncProcessor.SuccessCallback<CameraPreviewData, Bitmap>() {
-        @Override public void handleResult(CameraPreviewData input, Bitmap output) {
-            overlayView.setFlipHorizontal(input.cameraInfo.isFrontFacing());
-            overlayView.setBitmap(output);
-            overlayView.invalidate();
-            if (DEBUG) {
-                long processingMillis = System.currentTimeMillis() - input.timestamp;
-                Log.i("AsciiCam", "Processed frame in : " + processingMillis + "ms");
-            }
-            finishFrame(input);
-        }
-    };
-
-    // Callback to handle an error producing a Bitmap.
-    AsyncProcessor.ErrorCallback<CameraPreviewData> errorCallback =
-            new AsyncProcessor.ErrorCallback<CameraPreviewData>() {
-        @Override public void handleException(CameraPreviewData input, Exception ex) {
-            Log.e(TAG, "Exception creating ascii image", ex);
-            finishFrame(input);
-        }
-    };
 
     void finishFrame(CameraPreviewData previewData) {
         CameraUtils.addPreviewCallbackBuffer(previewData.camera, previewData.pixelData);
@@ -540,5 +507,30 @@ implements Camera.PreviewCallback, ShutterButton.OnShutterButtonListener {
             return true;
         }
         return super.onKeyDown(keyCode, event);
+    }
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+
+
+    // Pixels and metadata for a preview frame.
+    static class CameraPreviewData {
+        public final Camera camera;
+        public final CameraUtils.CameraInfo cameraInfo;
+        public final byte[] pixelData;
+        public final int width;
+        public final int height;
+        public final long timestamp;
+
+        public CameraPreviewData(Camera c, CameraUtils.CameraInfo i, byte[] p, int w, int h, long t) {
+            camera = c;
+            cameraInfo = i;
+            pixelData = p;
+            width = w;
+            height = h;
+            timestamp = t;
+        }
     }
 }
